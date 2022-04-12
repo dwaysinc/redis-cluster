9a2dae40e7c0daf72c299860aa414a35211abe1d
allngth <dmitrii@dways.org>
2022-04-12 22:15:06 +0100

    node selection depends on strategy

--- src/Node.php
+++ src/Node.php
@@ -7,6 +7,10 @@ use Amp\Promise;
 use Amp\Redis\Redis;
 use RuntimeException;
 
+/**
+ * @see Redis
+ * @method set($key, $value, $options)
+ */
 final class Node
 {
     private Redis $redis;
@@ -14,6 +18,7 @@ final class Node
 
     /** @var Node[] */
     private array $slaveNodes = [];
+    private ?Node $masterNode = null;
 
     public function __construct(Redis $redis)
     {
@@ -64,5 +69,34 @@ final class Node
     public function setSlaveNodes(array $slaveNodes): void
     {
         $this->slaveNodes = $slaveNodes;
+    }
+
+    /**
+     * @return Node|null
+     */
+    public function getMasterNode(): ?Node
+    {
+        return $this->masterNode;
+    }
+
+    /**
+     * @param Node|null $masterNode
+     */
+    public function setMasterNode(?Node $masterNode): void
+    {
+        $this->masterNode = $masterNode;
+    }
+
+    public function isValidHashSlot(int $hashSlot): bool
+    {
+        foreach ($this->getNodeInfo()->getSlot() as $slot) {
+            $slot = explode('-', $slot);
+
+            if ($slot[0] <= $hashSlot && $slot[1] >= $hashSlot) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- src/RedisCluster.php
+++ src/RedisCluster.php
@@ -2,9 +2,12 @@
 
 namespace DwaysInc\RedisCluster;
 
+use Amp\Failure;
 use Amp\Promise;
 use Amp\Redis\Redis;
 use Amp\Redis\SetOptions;
+use RuntimeException;
+use Throwable;
 use function Amp\call;
 
 final class RedisCluster implements RedisClusterInterface
@@ -28,10 +31,26 @@ final class RedisCluster implements RedisClusterInterface
         }
     }
 
-    public function set(string $key, string $value, SetOptions $options = null): Promise
+    public function set(string $key, string $value, SetOptions $options = null, FindHashSlotStrategyEnum $findHashSlotStrategyEnum = FindHashSlotStrategyEnum::MASTER_FIRST): Promise
     {
-        return call(function () {
+        return call(function () use ($key, $findHashSlotStrategyEnum, $value, $options) {
             yield $this->connect();
+
+            // todo: refactor
+            $nodes = $this->getNodesByKey($key, $findHashSlotStrategyEnum);
+
+            foreach ($nodes as $node) {
+                try {
+                    return yield $node->set($key, $value, $options);
+                } catch (Throwable $e) {}
+            }
+
+            if (empty($nodes)) {
+                $e = new RuntimeException('Nodes not found');
+            }
+
+            // todo: refactor
+            return new Failure($e);
         });
     }
 
@@ -77,13 +96,96 @@ final class RedisCluster implements RedisClusterInterface
             }
         }
 
-        foreach ($slaveNodes as $node) {
-            if (isset($masterNodes[$node->getNodeInfo()->getMaster()])) {
-                $node->setSlaveNodes(array_merge($node->getSlaveNodes(), [$node->getNodeInfo()->getId() => $node]));
+        foreach ($slaveNodes as $slaveNode) {
+            if (isset($masterNodes[$slaveNode->getNodeInfo()->getMaster()])) {
+                $masterNode = $masterNodes[$slaveNode->getNodeInfo()->getMaster()];
+                $currentSlaves = $masterNode->getSlaveNodes();
+                $masterNode->setSlaveNodes(array_merge($currentSlaves, [$slaveNode->getNodeInfo()->getId() => $slaveNode]));
+                $slaveNode->setMasterNode($masterNode);
             }
         }
 
         $this->activeMasterNodes = $masterNodes;
         $this->activeSlaveNodes = $slaveNodes;
+    }
+
+    /**
+     * @param string $key
+     * @param FindHashSlotStrategyEnum $findHashSlotStrategyEnum
+     *
+     * @return Node[]
+     */
+    private function getNodesByKey(string $key, FindHashSlotStrategyEnum $findHashSlotStrategyEnum): array
+    {
+        $hashSlot = Crc16::calculate($key) & 0x3FFF;
+
+        $availableNodes = [];
+
+        switch ($findHashSlotStrategyEnum) {
+            case FindHashSlotStrategyEnum::MASTER_ONLY:
+                foreach ($this->activeMasterNodes as $id => $node) {
+                    if ($node->isValidHashSlot($hashSlot)) {
+                        $availableNodes[$id] = $node;
+
+                        break;
+                    }
+                }
+
+                break;
+            case FindHashSlotStrategyEnum::SLAVE_ONLY:
+                foreach ($this->activeMasterNodes as $node) {
+                    if ($node->isValidHashSlot($hashSlot) && !empty($node->getSlaveNodes())) {
+                        $availableNodes = array_merge($availableNodes, $node->getSlaveNodes());
+
+                        break;
+                    }
+                }
+
+                foreach ($this->activeSlaveNodes as $node) {
+                    if ($node->isValidHashSlot($hashSlot)) {
+                        $availableNodes = array_merge($availableNodes, [$node]);
+                    }
+                }
+
+                break;
+            case FindHashSlotStrategyEnum::MASTER_FIRST:
+                foreach ($this->activeMasterNodes as $id => $node) {
+                    if ($node->isValidHashSlot($hashSlot)) {
+                        $availableNodes[$id] = $node;
+                        $availableNodes = array_merge($availableNodes, $node->getSlaveNodes());
+
+                        break;
+                    }
+                }
+
+                foreach ($this->activeSlaveNodes as $id => $node) {
+                    if ($node->isValidHashSlot($hashSlot)) {
+                        $availableNodes[$id] = $node;
+                    }
+                }
+
+                break;
+            case FindHashSlotStrategyEnum::SLAVE_FIRST:
+                foreach ($this->activeSlaveNodes as $id => $node) {
+                    if ($node->isValidHashSlot($hashSlot)) {
+                        $availableNodes[$id] = $node;
+                    }
+                }
+
+                foreach ($this->activeMasterNodes as $id => $node) {
+                    if ($node->isValidHashSlot($hashSlot)) {
+                        $availableNodes = array_merge($availableNodes, $node->getSlaveNodes());
+                        $availableNodes[$id] = $node;
+
+                        break;
+                    }
+                }
+
+                break;
+            default:
+                throw new RuntimeException(sprintf('Unexpected FindHashSlotStrategyEnum given %s', $findHashSlotStrategyEnum->name));
+        }
+
+        return $availableNodes;
     }
 }
